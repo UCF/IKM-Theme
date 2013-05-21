84194bc22b82daa769a8781f2dce8372da12a955
Craig Froehlich <craigf@outlook.com>
2013-05-21 12:05:35 -0400

    Fixed bug with copied code had to remove breadcrumbs

--- template-two-column.php
+++ template-two-column.php
@@ -6,7 +6,6 @@
 <?php get_header(); the_post();?>
 	<div class="row page-content" id="<?=$post->post_name?>">
 		<div class="span12" id="page-top">
-			<?=get_breadcrumbs($post->ID)?>
 			
 			<? if(!is_front_page())	{ ?>
 				<h1><?php the_title();?></h1>
