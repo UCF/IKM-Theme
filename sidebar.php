ea7c8264aa4853fcc5934b948e6b9d8a1418c214
Craig Froehlich <craigf@outlook.com>
2013-06-25 16:01:10 -0400

    testing new menu

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,15 @@
 	
 	<p class="post-search-header-text">Search This Section</p>
 	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
-					
+	<?=wp_nav_menu(array(
+		'theme_location' => 'footer-menu', 
+		'container' => 'false', 
+		'menu_class' => 'menu vertical', 
+		'menu_id' => $taxonomy_term, 
+		'fallback_cb' => false,
+		'depth' => 1,
+		'walker' => new Bootstrap_Walker_Nav_Menu()
+		));
+	?>
 
 <?php endif;?>
