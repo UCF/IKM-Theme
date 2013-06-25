39962b35d1228ae400f0a7c992759e11437dbccf
Craig Froehlich <craigf@outlook.com>
2013-06-25 16:04:08 -0400

    Testing sidebar menu take 2

--- sidebar.php
+++ sidebar.php
@@ -7,10 +7,11 @@
 	<p class="post-search-header-text">Search This Section</p>
 	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
 	<?=wp_nav_menu(array(
-		'theme_location' => 'footer-menu', 
+		'theme_location' => '',
+	    'menu' => $taxonomy_term,
 		'container' => 'false', 
 		'menu_class' => 'menu vertical', 
-		'menu_id' => $taxonomy_term, 
+		'menu_id' => 'sidebar-menu', 
 		'fallback_cb' => false,
 		'depth' => 1,
 		'walker' => new Bootstrap_Walker_Nav_Menu()
