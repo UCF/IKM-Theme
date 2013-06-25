db7b8ba895bb67168448a85b0714a61b3dd099e5
Craig Froehlich <craigf@outlook.com>
2013-06-25 16:28:32 -0400

    Sidebar menu update

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 	<?php $taxonomy_term = get_post_meta($post->ID, 'page_taxonomy_term', TRUE) ? get_post_meta($post->ID, 'page_taxonomy_term', TRUE) : $post->post_name; ?>
 	
 	<p class="post-search-header-text">Search This Section</p>
-	
+	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
 	<?=wp_nav_menu(array(
 		'theme_location' => '',
 	    'menu' => $taxonomy_term,
@@ -13,7 +13,7 @@
 		'menu_class' => 'menu vertical', 
 		'menu_id' => 'sidebar-menu', 
 		'fallback_cb' => false,
-		'depth' => 1,
+		'depth' => 0,
 		'walker' => new Bootstrap_Walker_Nav_Menu(),
 		'items_wrap' => '<ul><li id="item-id">'.$taxonomy_term.'</li>%3$s</ul>'
 		));
