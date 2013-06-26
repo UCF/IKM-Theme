6576a807d3bfd257d9abae806ab3b74189e43e6c
Craig Froehlich <craigf@outlook.com>
2013-06-26 11:18:52 -0400

    Added title back in

--- template-two-column.php
+++ template-two-column.php
@@ -9,7 +9,9 @@
 			<div class="row page-content" id="<?=$post->post_name?>">
 				<div class="span12" id="page-top">
 					<article>
-						
+						<? if(!is_front_page())	{ ?>
+							<h2><?php the_title();?></h2>
+						<? } ?>						
 						<?php if (get_post_meta($post->ID, 'page_subheader', TRUE)) {
 							print get_post_meta($post->ID, 'page_subheader', TRUE);
 						}
