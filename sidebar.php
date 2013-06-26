b57ce98c0e34a5eeb2a1c2004f6f55cc983db994
Craig Froehlich <craigf@outlook.com>
2013-06-26 11:05:49 -0400

    added nav-list style overrides

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,8 @@
 <?php disallow_direct_load('sidebar.php');?>
 
 <?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Sidebar')):?>
-
 	<?php $taxonomy_term = get_post_meta($post->ID, 'page_taxonomy_term', TRUE) ? get_post_meta($post->ID, 'page_taxonomy_term', TRUE) : $post->post_name; ?>
 	
-	<p class="post-search-header-text">Search This Section</p>
 	<?//=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
 	<div id="sidbar-nav">
 	<?=wp_nav_menu(array(
@@ -12,7 +10,6 @@
 	    'menu' => $taxonomy_term,
 		'manu_class' => 'nav nav-list',
 		'container' => 'false', 
-		'menu_class' => 'menu vertical', 
 		'menu_id' => 'sidebar-menu', 
 		'fallback_cb' => false,
 		'depth' => 0,
