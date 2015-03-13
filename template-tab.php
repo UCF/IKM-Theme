58aca2492beb5b9528561a8d1290bfe0022fcee0
aswatts <aswatts@gmail.com>
2015-03-13 15:31:32 -0400

    tableau template changed

--- template-tab.php
+++ template-tab.php
@@ -14,7 +14,7 @@
                     <? } ?>
                     <?php the_content();?>
 			<hr />	
-<script type='text/javascript' src='https://public.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 1024px; height: 720px;'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;IP&#47;IPEDSDoctoralDegreesAwardedHistorical&#47;RaceandGenderseparated&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' width='1024' height='720' style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='site_root' value='' /><param name='name' value='IPEDSDoctoralDegreesAwardedHistorical&#47;RaceandGenderseparated' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;IP&#47;IPEDSDoctoralDegreesAwardedHistorical&#47;RaceandGenderseparated&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='showVizHome' value='no' /><param name='showTabs' value='y' /></object></div>
+
 		
 		<style>
 			.tableauTabbedNaviagtion .dijitTab {
