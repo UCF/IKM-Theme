0b04312e9e2412dd9a3edf19d41da8b1a6bdcabe
Andre Watts <aswatts@gmail.com>
2019-01-28 11:17:06 -0500

    Glossary php error find

--- includes/glossary.php
+++ includes/glossary.php
@@ -1,9 +1,10 @@
 <?php
 header('Content-type: application/xml');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+#ini_set('display_startup_errors', 1);
+ini_set('error_reporting',E_ALL);
+ini_set('display_errors','On');
 
 $xmlContent = '';
 
