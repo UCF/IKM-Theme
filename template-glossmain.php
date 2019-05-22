333e58e2518401ae551e2128f5d5078ee2500dbc
Andre Watts <aswatts@gmail.com>
2019-05-21 20:27:22 -0400

    addition of public tag parsing v2

--- template-glossmain.php
+++ template-glossmain.php
@@ -65,7 +65,7 @@ $(document).ready(function() {
 					$('#main').DataTable({
 						"columnDefs":[
 							{ className: "defStyle", targets: 1 }
-						], 												
+						], 										
 						"data": definitions,
 						"lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "Show All"]],
 						"paging":true,
